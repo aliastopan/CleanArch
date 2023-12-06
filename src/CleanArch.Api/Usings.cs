caa68ffeb954b4bac215df01895fab3c310a51fb
Taufan Augusta <taufan.augusta@protonmail.com>
2023-12-06 14:46:29 +0700

    add global usings

--- src/CleanArch.Api/Usings.cs
+++ src/CleanArch.Api/Usings.cs
@@ -1,6 +1,8 @@
 global using CleanArch.Api.Extensions;
 global using CleanArch.Api.Interfaces;
 
+global using Microsoft.AspNetCore.Mvc;
+
 global using AnnotatedResult;
 global using Mediator;
 global using Serilog;
--- src/CleanArch.Application/Usings.cs
+++ src/CleanArch.Application/Usings.cs
@@ -1,3 +1,5 @@
+global using CleanArch.Application.Common.Interfaces;
+
 global using AnnotatedResult;
 global using Mediator;
 global using Serilog;
