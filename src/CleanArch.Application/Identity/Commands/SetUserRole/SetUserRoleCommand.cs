53ed4fdbdefd9ecc04606ad93fb37c7d6d1bf281
Taufan Augusta <taufan.augusta@protonmail.com>
2023-12-20 18:27:18 +0700

    remove set user role

--- src/CleanArch.Application/Identity/Commands/SetUserRole/SetUserRoleCommand.cs
+++
@@ -1,6 +0,0 @@
-namespace CleanArch.Application.Identity.Commands.SetUserRole;
-
-public record SetUserRoleCommand(Guid GrantorId,
-    string PermissionPassword,
-    Guid GranteeId,
-    int Role) : IRequest<Result>;
--- src/CleanArch.Contracts/Identity/SetRoleRequest.cs
+++
@@ -1,6 +0,0 @@
-namespace CleanArch.Contracts.Identity;
-
-public record SetRoleRequest(Guid GrantorId,
-    string PermissionPassword,
-    Guid GranteeId,
-    int Role);
