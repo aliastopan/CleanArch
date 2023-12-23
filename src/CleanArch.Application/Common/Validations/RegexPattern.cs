b67d5528d49d0e3aa8cd3c9841a20c00ff8daaad
Taufan Augusta <taufan.augusta@protonmail.com>
2023-12-23 11:53:02 +0700

    add name format regex

--- src/CleanArch.Application/Common/Validations/RegexPattern.cs
+++ src/CleanArch.Application/Common/Validations/RegexPattern.cs
@@ -3,5 +3,6 @@ namespace CleanArch.Application.Common.Validations;
 public static class RegexPattern
 {
     public const string Username = "^(?=.{3,16}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$";
+    private const string NameFormat = "^[A-Za-z]+([-'][A-Za-z]+)*$";
     public const string StrongPassword = @"^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$";
 }
