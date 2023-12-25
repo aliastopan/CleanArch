0a0eea0cce8338a08541c3a9e37376b3f542f1e1
Taufan Augusta <taufan.augusta@protonmail.com>
2023-12-25 12:34:46 +0700

    delete starting endpoint

--- src/CleanArch.Api/Endpoints/Common/StartingEndpoint.cs
+++
@@ -1,9 +0,0 @@
-namespace CleanArch.Api.Endpoints.Common;
-
-public class StartingEndpoint : IEndpointDefinition
-{
-    public void DefineEndpoints(WebApplication app)
-    {
-        app.Map("api/start", _ => throw new NotImplementedException()).AllowAnonymous();
-    }
-}
