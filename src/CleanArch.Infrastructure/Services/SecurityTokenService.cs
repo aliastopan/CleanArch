0c402a3708f41847f7812c3bfb63310b10755e2a
Taufan Augusta <taufan.augusta@protonmail.com>
2023-12-26 12:01:12 +0700

    chore: clean up

--- src/CleanArch.Infrastructure/Services/SecurityTokenService.cs
+++ src/CleanArch.Infrastructure/Services/SecurityTokenService.cs
@@ -3,7 +3,6 @@ using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
-using Microsoft.EntityFrameworkCore;
 using CleanArch.Domain.Aggregates.Identity;
 
 namespace CleanArch.Infrastructure.Services;
