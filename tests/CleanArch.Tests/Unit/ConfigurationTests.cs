4ec887e1e7db9973aab103cc347fc33e2fa2476a
Taufan Augusta <taufan.augusta@protonmail.com>
2024-01-01 17:59:03 +0700

    tests: configuration

---
+++ tests/CleanArch.Tests/Unit/ConfigurationTests.cs
@@ -0,0 +1,30 @@
+using Microsoft.Extensions.Configuration;
+
+namespace CleanArch.Tests.Unit;
+
+public class ConfigurationTests : UnitTest
+{
+    [Fact]
+    public void Configuration_ShouldUseInMemoryDatabase()
+    {
+        Configuration.GetValue<bool>("UseInMemoryDatabase")
+            .Should()
+            .BeTrue();
+    }
+
+    [Fact]
+    public void SecurityTokenSettings_Issuer_ShouldBeCleanArch()
+    {
+        Configuration["SecurityToken:Issuer"]
+            .Should()
+            .Be("CleanArch");
+    }
+
+    [Fact]
+    public void SecurityTokenSettings_Audience_ShouldBeCleanArch()
+    {
+        Configuration["SecurityToken:Audience"]
+            .Should()
+            .Be("CleanArch");
+    }
+}
