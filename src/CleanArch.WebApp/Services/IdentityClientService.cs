9ada47c6f0df3c48286b08e29fe523802cd937c5
Taufan Augusta <taufan.augusta@protonmail.com>
2024-01-07 15:34:14 +0700

    refactor: return 'HttpResult'

--- src/CleanArch.WebApp/Services/IdentityClientService.cs
+++ src/CleanArch.WebApp/Services/IdentityClientService.cs
@@ -1,8 +1,6 @@
 using System.Text;
 using System.Text.Json;
 using AnnotatedResult;
-using Serilog;
-using CleanArch.Shared.Contracts;
 using CleanArch.Shared.Contracts.Identity;
 
 namespace CleanArch.WebApp.Services;
@@ -16,39 +14,19 @@ public sealed class IdentityClientService
         _httpClient = httpClient;
     }
 
-    public async Task<Result<SignInResponse>> SignInAsync(string username, string password)
+    public async Task<HttpResult> SignInAsync(string username, string password)
     {
         var request = new SignInRequest(username, password);
         var bodyString = JsonSerializer.Serialize(request);
         var content = new StringContent(bodyString, Encoding.UTF8, "application/json");
 
-
         using var responseMessage = await _httpClient.PostAsync("api/sign-in", content);
 
-        string responseContent;
-        var options = new JsonSerializerOptions
+        return new HttpResult
         {
-            PropertyNameCaseInsensitive = true
+            IsSuccessStatusCode = responseMessage.IsSuccessStatusCode,
+            Headers = responseMessage.Headers,
+            Content = await responseMessage.Content.ReadAsStringAsync()
         };
-
-        if (!responseMessage.IsSuccessStatusCode)
-        {
-            responseContent = await responseMessage.Content.ReadAsStringAsync();
-            var statusCode = responseMessage.StatusCode;
-            var problemDetails = JsonSerializer.Deserialize<ProblemDetailsResponse>(responseContent, options);
-
-            Log.Fatal("Status Code {0}", responseMessage.StatusCode);
-            Log.Fatal("Content {0}", responseContent);
-            Log.Fatal("Problem Details {0}", problemDetails);
-
-            return Result<SignInResponse>.Error((int)statusCode);
-        }
-
-        responseContent = await responseMessage.Content.ReadAsStringAsync();
-        var response = JsonSerializer.Deserialize<SignInResponse>(responseContent, options);
-        Log.Fatal("Content {0}", responseContent);
-        Log.Fatal("Access Token {0}", response!.AccessToken);
-
-        return Result<SignInResponse>.Ok(response!);
     }
 }
