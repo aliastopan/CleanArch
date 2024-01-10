694e4e9a0efbd12efe422428a5f1c0784dc1f39e
Taufan Augusta <taufan.augusta@protonmail.com>
2024-01-10 17:36:40 +0700

    feat: seamless authentication with security token

--- src/CleanArch.WebApp/Security/CustomAuthenticationStateProvider.cs
+++ src/CleanArch.WebApp/Security/CustomAuthenticationStateProvider.cs
@@ -2,6 +2,7 @@ using System.Security.Claims;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
 using CleanArch.Application.Common.Interfaces.Services;
+using CleanArch.WebApp.Services;
 
 namespace CleanArch.WebApp.Security;
 
@@ -9,32 +10,68 @@ public sealed class CustomAuthenticationStateProvider : AuthenticationStateProvi
 {
     private readonly IAccessTokenService _accessTokenService;
     private readonly ProtectedSessionStorage _sessionStorage;
+    private readonly IdentityClientService _identityClientService;
 
     public CustomAuthenticationStateProvider(IAccessTokenService accessTokenService,
-        ProtectedSessionStorage sessionStorage)
+        ProtectedSessionStorage sessionStorage,
+        IdentityClientService identityClientService)
     {
         _accessTokenService = accessTokenService;
         _sessionStorage = sessionStorage;
+        _identityClientService = identityClientService;
     }
 
     public override async Task<AuthenticationState> GetAuthenticationStateAsync()
     {
-        var tryAuth = await TryAuthenticateAsync();
+        var tryGetAccessToken = await TryGetAccessTokenAsync();
+        var tryGetRefreshToken = await TryGetRefreshTokenAsync();
 
-        if (tryAuth.IsFailure)
+        if (tryGetAccessToken.IsFailure || tryGetRefreshToken.IsFailure)
         {
-            return new AuthenticationState(new ClaimsPrincipal());
+            Log.Fatal("Unauthorized.");
+            return UnauthorizedState();
         }
 
-        var accessToken = tryAuth.Value;
+        var accessToken = tryGetAccessToken.Value;
+        var refreshToken = tryGetRefreshToken.Value;
+
         var principal = _accessTokenService.GetPrincipalFromToken(accessToken);
+        if (principal is null)
+        {
+            Log.Fatal("Unauthorized.");
+            return UnauthorizedState();
+        }
+
+        Log.Warning("Authenticating...");
+        var tryAuthenticate = _accessTokenService.TryValidateAccessToken(accessToken);
+        if (tryAuthenticate.IsFailure)
+        {
+            Log.Warning("Authentication failed.");
+            Log.Warning("Trying to refresh authentication.");
+            var response = await _identityClientService.RefreshAccessAsync(accessToken, refreshToken);
+            Log.Warning("Response: {0}", response.IsSuccessStatusCode);
+            Log.Warning("Response: {0}", response.Content);
+            if (response.IsSuccessStatusCode)
+            {
+                Log.Warning("Refresh authentication has successful.");
+                await _sessionStorage.SetAsync("access-token", accessToken);
+                await _sessionStorage.SetAsync("refresh-token", refreshToken);
+            }
+            else
+            {
+                Log.Fatal("Refresh token has expired");
+                Log.Fatal("Unauthorized.");
+                return UnauthorizedState();
+            }
+        }
+
         var authenticationState = new AuthenticationState(principal);
 
         NotifyAuthenticationStateChanged(Task.FromResult(authenticationState));
         return authenticationState;
     }
 
-    private async Task<Result<string>> TryAuthenticateAsync()
+    private async Task<Result<string>> TryGetAccessTokenAsync()
     {
         var result = await _sessionStorage.GetAsync<string>("access-token");
 
@@ -45,5 +82,23 @@ public sealed class CustomAuthenticationStateProvider : AuthenticationStateProvi
         }
 
         return Result<string>.Error();
+    }
+
+    private async Task<Result<string>> TryGetRefreshTokenAsync()
+    {
+        var result = await _sessionStorage.GetAsync<string>("refresh-token");
+
+        if (result.Success)
+        {
+            var accessToken = result.Value;
+            return Result<string>.Ok(accessToken!);
+        }
+
+        return Result<string>.Error();
+    }
+
+    private static AuthenticationState UnauthorizedState()
+    {
+        return new AuthenticationState(new ClaimsPrincipal());
     }
 }
