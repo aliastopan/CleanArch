7a3140a460114ac20235ecf2193d49a45eac873b
Taufan Augusta <taufan.augusta@protonmail.com>
2024-01-17 21:56:11 +0700

    pre-migrations

--- src/CleanArch.Api/Extensions/WebApplicationExtensions.cs
+++ src/CleanArch.Api/Extensions/WebApplicationExtensions.cs
@@ -6,6 +6,12 @@ public static class WebApplicationExtensions
 {
     public static void InitializeDbContext(this WebApplication app)
     {
+        var configuration = app.Services.GetRequiredService<IConfiguration>();
+        if (!configuration.UseInMemoryDatabase())
+        {
+            return;
+        }
+
         using var scope = app.Services.CreateScope();
         var task = scope.ServiceProvider.GetRequiredService<IAppDbContextSeeder>().GenerateUsersAsync();
         task.GetAwaiter().GetResult();
--- src/CleanArch.Api/Usings.cs
+++ src/CleanArch.Api/Usings.cs
@@ -3,6 +3,7 @@ global using CleanArch.Api.Interfaces;
 global using CleanArch.Api.Logging;
 global using CleanArch.Api.Middlewares;
 global using CleanArch.Api.Security;
+global using CleanArch.Infrastructure.Extensions;
 
 global using Microsoft.AspNetCore.Mvc;
 
