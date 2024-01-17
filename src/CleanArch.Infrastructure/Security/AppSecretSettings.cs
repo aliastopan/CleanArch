415a1618b51d725ed33a0ce4f51d8e52a8cc8153
Taufan Augusta <taufan.augusta@protonmail.com>
2024-01-17 12:58:48 +0700

    add connection string to 'AppSecret'

--- src/CleanArch.Infrastructure/Security/AppSecretSettings.cs
+++ src/CleanArch.Infrastructure/Security/AppSecretSettings.cs
@@ -5,9 +5,11 @@ public class AppSecretSettings
     public static class Element
     {
         public const string MasterKey = "AppSecrets:MasterKey";
+        public const string ConnectionString = "AppSecrets:ConnectionString";
     }
 
     public const string SectionName = "AppSecrets";
 
     public string MasterKey { get; set; } = "";
+    public string ConnectionString { get; set; } = "";
 }
