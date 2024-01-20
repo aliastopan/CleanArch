a1bb823f42544bbf2053e9b74dd61272a2aa82af
Taufan Augusta <taufan.augusta@protonmail.com>
2024-01-20 11:24:38 +0700

    add global usings

--- src/CleanArch.Infrastructure/Usings.cs
+++ src/CleanArch.Infrastructure/Usings.cs
@@ -1,10 +1,10 @@
 global using CleanArch.Application;
 global using CleanArch.Application.Common.Interfaces.Managers;
-global using CleanArch.Application.Common.Extensions.Repository;
 global using CleanArch.Application.Common.Interfaces.Persistence;
 global using CleanArch.Application.Common.Interfaces.Services;
 global using CleanArch.Application.Common.Interfaces.Services.Aggregates;
 global using CleanArch.Infrastructure.Extensions;
+global using CleanArch.Infrastructure.Extensions.Repository;
 global using CleanArch.Infrastructure.Managers;
 global using CleanArch.Infrastructure.Persistence;
 global using CleanArch.Infrastructure.Security;
