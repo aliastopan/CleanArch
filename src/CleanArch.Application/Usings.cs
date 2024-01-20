17a80faf2a2dbdf450fd9a1338d58942cd5d8796
Taufan Augusta <taufan.augusta@protonmail.com>
2024-01-20 11:36:51 +0700

    remove deprecated namespace

--- src/CleanArch.Application/Usings.cs
+++ src/CleanArch.Application/Usings.cs
@@ -1,7 +1,6 @@
 global using CleanArch.Application.Common.Interfaces.Managers;
 global using CleanArch.Application.Common.Interfaces.Persistence;
 global using CleanArch.Application.Common.Interfaces.Services;
-global using CleanArch.Application.Common.Extensions.Repository;
 global using CleanArch.Shared.Validations;
 
 global using AnnotatedResult;
