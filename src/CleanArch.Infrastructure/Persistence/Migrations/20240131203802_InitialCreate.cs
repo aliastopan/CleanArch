5d778264a3fa8932bae5cfc6cba62b1f1894568b
Taufan Augusta <taufan.augusta@protonmail.com>
2024-02-01 10:48:07 +0700

    migrations: 'cleanarch' db

---
+++ src/CleanArch.Infrastructure/Persistence/Migrations/20240131203802_InitialCreate.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CleanArch.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "dbo.user_account",
+                columns: table => new
+                {
+                    user_account_id = table.Column<Guid>(type: "char(36)", maxLength: 36, nullable: false, collation: "ascii_general_ci"),
+                    username = table.Column<string>(type: "varchar(16)", maxLength: 16, nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    email_address = table.Column<string>(type: "varchar(255)", maxLength: 255, nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    user_role = table.Column<int>(type: "int", nullable: true),
+                    user_privileges = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    is_verified = table.Column<bool>(type: "tinyint(1)", nullable: false),
+                    password_hash = table.Column<string>(type: "varchar(96)", maxLength: 96, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    password_salt = table.Column<string>(type: "varchar(16)", maxLength: 16, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    creation_date = table.Column<DateTimeOffset>(type: "datetime(6)", nullable: false),
+                    last_signed_in = table.Column<DateTimeOffset>(type: "datetime(6)", nullable: false),
+                    fk_user_profile_id = table.Column<Guid>(type: "char(36)", nullable: false, collation: "ascii_general_ci")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_dbo.user_account", x => x.user_account_id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "dbo.refresh_token",
+                columns: table => new
+                {
+                    refresh_token_id = table.Column<Guid>(type: "char(36)", maxLength: 36, nullable: false, collation: "ascii_general_ci"),
+                    token = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    jti = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    creation_date = table.Column<DateTimeOffset>(type: "datetime(6)", nullable: false),
+                    expiry_date = table.Column<DateTimeOffset>(type: "datetime(6)", nullable: false),
+                    is_used = table.Column<bool>(type: "tinyint(1)", nullable: false),
+                    is_invalidated = table.Column<bool>(type: "tinyint(1)", nullable: false),
+                    is_deleted = table.Column<bool>(type: "tinyint(1)", nullable: false),
+                    fk_user_account_id = table.Column<Guid>(type: "char(36)", nullable: false, collation: "ascii_general_ci")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_dbo.refresh_token", x => x.refresh_token_id);
+                    table.ForeignKey(
+                        name: "FK_dbo.refresh_token_dbo.user_account_fk_user_account_id",
+                        column: x => x.fk_user_account_id,
+                        principalTable: "dbo.user_account",
+                        principalColumn: "user_account_id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "dbo.user_profile",
+                columns: table => new
+                {
+                    user_profile_id = table.Column<Guid>(type: "char(36)", maxLength: 36, nullable: false, collation: "ascii_general_ci"),
+                    first_name = table.Column<string>(type: "varchar(64)", maxLength: 64, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    last_name = table.Column<string>(type: "varchar(64)", maxLength: 64, nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    date_of_birth = table.Column<DateOnly>(type: "date", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_dbo.user_profile", x => x.user_profile_id);
+                    table.ForeignKey(
+                        name: "FK_dbo.user_profile_dbo.user_account_user_profile_id",
+                        column: x => x.user_profile_id,
+                        principalTable: "dbo.user_account",
+                        principalColumn: "user_account_id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_dbo.refresh_token_fk_user_account_id",
+                table: "dbo.refresh_token",
+                column: "fk_user_account_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "dbo.refresh_token");
+
+            migrationBuilder.DropTable(
+                name: "dbo.user_profile");
+
+            migrationBuilder.DropTable(
+                name: "dbo.user_account");
+        }
+    }
+}
